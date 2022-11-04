cc7d45b9ec53967c4295b0ba6ee3f7a7f94df84b
Dmitrii Klypo <dmitrii.klypo@maiil.ru>
2022-11-04 11:14:25 +0400

    add header and footer

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -3,6 +3,9 @@ import TabsWrapper from '@/components/TabsWrapper.vue'
 import TabItem from '@/components/Tab.vue'
 import axios from 'axios'
 import {ref} from 'vue'
+import Layout from '@//components/Layout'
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 const post = ref([
 
 ])
@@ -29,17 +32,21 @@ const fetchText = async () => {
 </script>
 
 <template>
-  <TabsWrapper>
-    <TabItem title="Tab 1" id="1">{{ post.shortText }} <button @click="fetchText">Test</button></TabItem>
-    <TabItem title="Tab 2" id="2">{{ post }} <button @click="fetchText">Test</button></TabItem>
-    <TabItem title="Tab 3" id="3">Content from Tab 3 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-      Voluptates,
-      ipsa.</TabItem>
-    <TabItem title="Tab 4" id="4">Content from Tab 4 Lorem ipsum dolor sit amet.</TabItem>
-    <TabItem title="Tab 5" id="5">Content from Tab 5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-      maiores ab minima. Facere mollitia tempora soluta, commodi beatae sit. Distinctio.</TabItem>
-    <TabItem title="Tab 6" id="6">Content from Tab 6</TabItem>
-  </TabsWrapper>
+  <Header/>
+  <Layout>
+    <TabsWrapper>
+      <TabItem title="Tab 1" id="1">{{ post }} <button @click="fetchText">Test</button></TabItem>
+      <TabItem title="Tab 2" id="2">{{ post }} <button @click="fetchText">Test</button></TabItem>
+      <TabItem title="Tab 3" id="3">Content from Tab 3 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Voluptates,
+        ipsa.</TabItem>
+      <TabItem title="Tab 4" id="4">Content from Tab 4 Lorem ipsum dolor sit amet.</TabItem>
+      <TabItem title="Tab 5" id="5">Content from Tab 5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+        maiores ab minima. Facere mollitia tempora soluta, commodi beatae sit. Distinctio.</TabItem>
+      <TabItem title="Tab 6" id="6">Content from Tab 6</TabItem>
+    </TabsWrapper>
+  </Layout>
+  <Footer/>
 </template>
 
 <style>

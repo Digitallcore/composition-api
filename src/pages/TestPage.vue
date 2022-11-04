d0c5520551898371b1c167b69a0eb0d72decd5a6
Dmitrii Klypo <dmitrii.klypo@maiil.ru>
2022-11-04 15:48:06 +0400

    replace link to header, remade tabs

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -6,10 +6,10 @@ import TabsWrapper from "@/components/TabsWrapper";
 
 <template>
   <TabsWrapper>
-    <Tab title="Title 1">Hello from Tab 1</Tab>
-    <Tab title="Title 2">Hello from Tab 2</Tab>
-    <Tab title="Title 3">Hello from Tab 3</Tab>
-    <Tab title="Title 4">Hello from Tab 4</Tab>
+    <Tab title="Title 1">Description 1</Tab>
+    <Tab title="Title 2">Description 2</Tab>
+    <Tab title="Title 3">Description 3</Tab>
+    <Tab title="Title 4">Description 4</Tab>
   </TabsWrapper>
   <RouterLink to='/' class="self-center bg-green-700 text-green-100 rounded p-1.5">Home</RouterLink>
 </template>

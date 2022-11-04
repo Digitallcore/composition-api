39d46659ba1e6eb84b4851e13993205debb1508a
Dmitrii Klypo <dmitrii.klypo@maiil.ru>
2022-11-04 10:52:32 +0400

    replace link to header, remade tabs

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,6 @@
     <footer
       class="px-4 py-8 text-sm font-bold text-center text-green-100 bg-green-900">
       <p class="text-sm tracking-wide">Footer</p>
-      <RouterLink to="Test">Reusable Tabs</RouterLink>
+
     </footer>
   </template>
